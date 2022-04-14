bc583b0b2cc3cf2ecf1037a4a7addd9e3292382c
fwc-wsl <1643806656@qq.com>
2022-04-15 01:11:44 +0800

    fix quick sort a bug

--- tree/binary_tree.rs
+++ tree/binary_tree.rs
@@ -2,24 +2,43 @@ use std::fmt::Debug;
 
 struct TreeNode<T: Debug> {
     data: T,
-    rchild: Option<Box<TreeNode<T>>>,
+    lchild: Option<Box<TreeNode<T>>>,
     rchild: Option<Box<TreeNode<T>>>,
 }
 
-struct Tree<T: Debug> {
-    node: Option<Box<TreeNode<T>>>,
-}
-
-fn preorder_traverse<T: Debug>(node: Tree<T>) {
-    if node.is_none() {
-        return;
+fn preorder_traverse<T: Debug>(node: TreeNode<T>) {
+    if node.lchild.is_some() && node.rchild.is_some() {
+        println!("{:?}", node.data);
+        preorder_traverse(node.lchild.unwrap());
+        preorder_traverse(node.rchild.unwrap());
     }
-    let node = node.node.unwrap();
-    println!("{:?}", node.data);
-    preorder_traverse(Tree { node: node.rchild });
-    preorder_traverse(Tree { node: node.lchild });
 }
 
 fn main() {
-    let mut tree = Tree { node: None };
+    let node5 = TreeNode {
+        data: 5,
+        lchild: None,
+        rchild: None,
+    };
+    let node4 = TreeNode {
+        data: 4,
+        lchild: None,
+        rchild: None,
+    };
+    let node3 = TreeNode {
+        data: 3,
+        lchild: None,
+        rchild: None,
+    };
+    let node2 = TreeNode {
+        data: 2,
+        lchild: Some(Box::new(node4)),
+        rchild: Some(Box::new(node5)),
+    };
+    let node1 = TreeNode {
+        data: 1,
+        lchild: Some(Box::new(node2)),
+        rchild: Some(Box::new(node3)),
+    };
+    preorder_traverse(node1);
 }
